ff31bfb623c8885756f5f3ecf6a3b9a5a22b0837
qingxin2016@gmail.com <qingxin2016@gmail.com>
2016-10-31 23:35:09 +0900

    add edit viev delete and add action Signed-off-by: qingxin2016@gmail.com <qingxin2016@gmail.com>

--- fuel/app/classes/controller/news.php
+++ fuel/app/classes/controller/news.php
@@ -2,6 +2,8 @@
 
 use Fuel\Core\Controller_Template;
 use Fuel\Core\Input;
+use Fuel\Core\Session;
+use Fuel\Core\Response;
 
 class Controller_News extends Controller_Template
 {
@@ -17,7 +19,11 @@ class Controller_News extends Controller_Template
 	
 	public function action_view($id)
 	{
-		$news = Model_news::find($id);
+		$news = Model_news::find('first',array(
+				'where'=>array(
+						'id'=>$id
+				)
+		));
 		
 		
 		
@@ -27,7 +33,26 @@ class Controller_News extends Controller_Template
 	}
 	public function action_edit($id)
 	{
-		$news = Model_news::find($id);
+		if (Input::post('send')){
+			$news =Model_news::find('post_id');
+			$news = Model_news::find($id);
+			$news->title=Input::post('title');
+			$news->body=Input::post('body');
+			$news->created_at=date('Y-m-d H:i:s');
+				
+			$news->save();
+				
+			Session::set_flash('success','The news Updated');
+				
+			Response::redirect('/');
+				
+				
+		}
+		$news = Model_news::find('first',array(
+				'where'=>array(
+						'id'=>$id
+				)
+		));
 		
 		
 		
@@ -37,9 +62,42 @@ class Controller_News extends Controller_Template
 	}
 	public function action_add()
 	{
+		if (Input::post('send')){
+			$news =new Model_news();
+			$news->title=Input::post('title');
+			$news->body=Input::post('body');
+			$news->created_at=date('Y-m-d H:i:s');
+			
+			$news->save();
+			
+			Session::set_flash('success','The news Added');
+			
+			Response::redirect('/');
+			
+			
+		}
+		
 		$data = array();
         $this->template->title = 'add';
         $this->template->content = View::forge('news/add', $data);
+	}
+	public function action_delete($id)
+	{
+		$news = Model_news::find('first',array(
+				'where'=>array(
+						'id'=>$id
+				)
+		));
+	
+		$news->delete();
+		
+		
+		Session::set_flash('success','The news deleted');
+			
+		Response::redirect('/');
+		
+	
+		
 	}
 
 	
