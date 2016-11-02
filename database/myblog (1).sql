c12436e21f271bb710f313a38c1b2f94867727b4
qingxin2016 <qingxin2016@gmail.com>
2016-11-02 18:42:48 +0900

    fix the uploaded images
    Signed-off-by: qingxin2016 <qingxin2016@gmail.com>

---
+++ database/myblog (1).sql
@@ -0,0 +1,156 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 2016 年 11 朁E02 日 10:40
+-- サーバのバージョン： 10.1.16-MariaDB
+-- PHP Version: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `myblog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `newsid` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `comment` text NOT NULL,
+  `file` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- テーブルのデータのダンプ `comments`
+--
+
+INSERT INTO `comments` (`id`, `newsid`, `email`, `comment`, `file`, `created_at`) VALUES
+(19, 3, '321@qqcom', 'dsadada', '38844a1e985466651f07286afea469c0.jpg', '2016-11-02 07:48:43'),
+(20, 1, 'qingxin2016@gmail.com', 'dsadadddddddddddddddddddddddd', '861bcc7b2e87d2d535407646cdddf8eb.jpg', '2016-11-02 08:27:12'),
+(21, 8, 'miyashitatohru@gmail.com', 'hhhhhhhhhh', '783936fd05d1b4aa4ad63617a3b87dbf.jpg', '2016-11-02 09:33:54'),
+(22, 1, '826955835@qq.com', 'yyyyyyyyy', '0ea9b17f1e3a94d335532d3ae6a528c3.jpg', '2016-11-02 09:54:49'),
+(23, 6, 'fff@qq.com', 'fdsf', 'fe51be6fc7a2a1ed2dc1726b74eb28ee.jpg', '2016-11-02 10:13:09'),
+(24, 6, '321@qqcom', '//////////////////////////////', 'de06002b891588e2e95ac39dfdd230a8.jpg', '2016-11-02 10:13:22'),
+(25, 6, 'qingxin2016@gmail.com', '222222222222', '3926c9acd7e06a5690647e5a18e796bc.jpg', '2016-11-02 10:15:00'),
+(26, 1, 'qingxin2016@gmail.com', 'fffffffffff', '', '2016-11-02 10:26:16'),
+(27, 7, 'miyashitatohru@gmail.com', 'tttttttttttt', 'e6a27239f7cfe9bd7ee8178cd494c1ca.jpg', '2016-11-02 10:36:12'),
+(28, 7, 'qingxin2016@gmail.com', 'Images in Bootstrap 3 can be made responsive-friendly via the addition of the .img-responsive class. This applies max-width: 100%;, height: auto; and display: block; to the image so that it scales nicely to the parent element.', '240a18a6e88efe93ffe8119185a0aacb.jpg', '2016-11-02 10:36:38'),
+(29, 8, 'qingxin2016@gmail.com', 'This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.', '11af8bd7e4c9d3cd64bc516499bbb8b8.jpg', '2016-11-02 10:39:51');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `files`
+--
+
+CREATE TABLE `files` (
+  `id` int(11) NOT NULL,
+  `commentid` int(11) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- テーブルのデータのダンプ `files`
+--
+
+INSERT INTO `files` (`id`, `commentid`, `path`, `created_at`) VALUES
+(9, 16, 'C:\\xampp\\htdocs\\myblog\\public\\files\\', '2016-11-02 07:41:50'),
+(10, 17, 'C:\\xampp\\htdocs\\myblog\\public\\files\\', '2016-11-02 07:42:15'),
+(11, 18, 'C:\\xampp\\htdocs\\myblog\\public\\files\\', '2016-11-02 07:46:52'),
+(12, 19, 'C:\\xampp\\htdocs\\myblog\\public\\files\\', '2016-11-02 07:48:43'),
+(13, 20, 'C:\\xampp\\htdocs\\myblog\\public\\files\\', '2016-11-02 08:27:12'),
+(14, 21, 'C:\\xampp\\htdocs\\myblog\\public\\files\\', '2016-11-02 09:33:54'),
+(15, 22, 'C:\\xampp\\htdocs\\myblog\\public\\files\\', '2016-11-02 09:54:49'),
+(16, 23, 'C:\\xampp\\htdocs\\myblog\\public\\files\\', '2016-11-02 10:13:09'),
+(17, 24, 'C:\\xampp\\htdocs\\myblog\\public\\files\\', '2016-11-02 10:13:22'),
+(18, 25, 'C:\\xampp\\tmp\\php2662.tmp', '2016-11-02 10:15:01'),
+(19, 27, 'C:\\xampp\\htdocs\\myblog\\public\\files\\', '2016-11-02 10:36:12'),
+(20, 28, 'C:\\xampp\\htdocs\\myblog\\public\\files\\', '2016-11-02 10:36:38'),
+(21, 29, 'C:\\xampp\\htdocs\\myblog\\public\\files\\', '2016-11-02 10:39:51');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `body` text NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- テーブルのデータのダンプ `news`
+--
+
+INSERT INTO `news` (`id`, `title`, `body`, `created_at`) VALUES
+(1, 'What is Lorem Ipsum!?', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2016-11-02 10:26:07'),
+(2, 'Why do we use it?', 't is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using ''Content here, content here'', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for ''lorem ipsum'' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).\r\n\r\n', '2016-10-31 17:45:39'),
+(3, 'What is Lorem Ipsum?', 'ustry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not on', '2016-11-01 07:09:54'),
+(5, 'Where can I get some?', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don''t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn''t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.', '2016-11-01 07:11:10'),
+(6, 'Where does it come from?', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.\r\n\r\nThe standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.', '2016-11-02 09:57:36'),
+(7, '1914 translation by H. Rackham', '"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"', '2016-11-01 07:12:36'),
+(8, 'Section 1.10.33 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC', '"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."', '2016-11-01 07:12:59');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `files`
+--
+ALTER TABLE `files`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+--
+-- AUTO_INCREMENT for table `files`
+--
+ALTER TABLE `files`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+--
+-- AUTO_INCREMENT for table `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
