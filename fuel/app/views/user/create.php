52ff88d378b145ee6d18299d29ed2251e97318fe
qingxin2016 <qingxin2016@gmail.com>
2016-11-07 19:03:13 +0900

    add the create button

---
+++ fuel/app/views/user/create.php
@@ -0,0 +1,10 @@
+<br><br>
+<div class="row">
+	<div class="col-lg-12">
+		<?php echo Form::open(array('action'=>'/user/create'));?>
+			
+			<br>
+			<?php echo Form::submit('send','Create',array('class'=>'btn btn-success'));?>
+		<?php echo Form::close();?>
+	</div>
+</div>
