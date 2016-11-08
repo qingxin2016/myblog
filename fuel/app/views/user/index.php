e0b1e292242a9ff15d18c78a3f656db9f4e52f93
qingxin2016 <qingxin2016@gmail.com>
2016-11-08 14:01:58 +0900

    add the function to delete the user

--- fuel/app/views/user/index.php
+++ fuel/app/views/user/index.php
@@ -7,25 +7,42 @@
 <div class="row">
 	<div class="col-lg-12">
 		<table class="table table-hover">
+		<colgroup>
+			<col style="width: 8%;">
+			<col style="width: 15%;">
+			<col style="width: 15%;">
+			<col style="width: 5%;">
+			<col style="width: 15%;">
+			<col style="width: 15%;">
+			<col style="width: 17%;">
+		</colgroup>
 		  <thead class="thead-default">
 		    <tr class="success">
-		      <th>#</th>
-		      <th>Username</th>
-		      <th>Email</th>
-		      <th>Group</th>
-		      <th>Last_login</th>
-		      <th>Created_at</th>
+		      <th class="text-center">ID</th>
+		      <th class="text-center">Username</th>
+		      <th class="text-center">Email</th>
+		      <th class="text-center">Group</th>
+		      <th class="text-center">Last_login</th>
+		      <th class="text-center">Created_at</th>
+		      <th></th>
 		    </tr>
 		  </thead>
 		  <tbody>
 		  <?php foreach ($data as $items):?>
 		    <tr>
-		      <th scope="row"><?php echo $items['id'];?></th>
+		      <th scope="row" class="text-center"><?php echo $items['id'];?></th>
 		      <td><?php echo $items['username'];?></td>
 		      <td><?php echo $items['email'];?></td>
-		      <td><?php echo $items['group'];?></td>
-		      <td><?php echo Date::forge($items['last_login']);?></td>
-		      <td><?php echo Date::forge($items['created_at']);?></td>
+		      <td class="text-center"><?php echo $items['group'];?></td>
+		      <td class="text-center"><?php echo Date::forge($items['last_login']);?></td>
+		      <td class="text-center"><?php echo Date::forge($items['created_at']);?></td>
+		      <td>
+		      	<div class="btn-group">
+				  <a type="button" class="btn btn-primary">Edit</a>
+				  <a type="button" class="btn btn-warning">Reset</a>
+				  <a type="button" class="btn btn-danger" href="/user/delete/<?php echo $items['username'];?>" onclick="javascript:return confirm('Are you sure to delete this news?')">Delete</a>
+				</div>
+		      </td>
 		    </tr>
 		    <?php endforeach;?>
 		  </tbody>
