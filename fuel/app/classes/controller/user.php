ac504b0df396ce776c031994501e33738e56c515
qingxin2016 <qingxin2016@gmail.com>
2016-11-08 10:45:55 +0900

    add the create user function

--- fuel/app/classes/controller/user.php
+++ fuel/app/classes/controller/user.php
@@ -1,26 +1,30 @@
 <?php
-
 use Fuel\Core\Controller_Template;
-
-class Controller_User extends Controller_Template
-{
-	
-	public function action_index()
-	{
-		$data['user']=Model_user::find('all');
+use Fuel\Core\Input;
+use Fuel\Core\Session;
+use Fuel\Core\Response;
+class Controller_User extends Controller_Template {
+	public function action_index() {
+		$data ['user'] = Model_user::find ( 'all' );
 		
-		$data= array(
-				'data'=>$data['user']
-		);		
+		$data = array (
+				'data' => $data ['user'] 
+		);
 		$this->template->title = 'User';
-		$this->template->content = View::forge('user/index',$data);
+		$this->template->content = View::forge ( 'user/index', $data );
 	}
-	public function action_create()
-	{
+	public function action_create() {
+		if (Input::post ( 'send' )) {
+			// 新しいユーザーを作成
+			Auth::create_user ( Input::post ( 'username' ), Input::post ( 'password' ), Input::post ( 'email' ), Input::post ( 'group' ) );
+			
+			Session::set_flash('success','New user has been created.');
+			Response::redirect('user/index');
+		}
 		
-		$this->template->title = 'User';
-		$this->template->content = View::forge('user/create');
+		
+		
+		$this->template->title = 'Create User';
+		$this->template->content = View::forge ( 'user/create' );
 	}
-
-	
 }
